c9cd3a49814c4841b3e3f1a76da90c4d547c9294
adityaKumar <adityakumarrajput01@gmail.com>
2023-12-26 23:41:59 +0530

    coins page and features

--- src/Components/Coins/Coins.jsx
+++ src/Components/Coins/Coins.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react'
+import axios from "axios";
+import Loading from '../Loading';
+import './Coins.css';
+import Header from '../Header/Header';
 
 const Coins = () => {
+    const [loading, setloading] = useState(true);
+    const [currency, setcurrency] = useState('inr');
+    const [coins, setcoins] = useState([]);
+    const currencySymbol = currency === 'inr' ? '₹' : '$'
+
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}`;
+    useEffect(() => {
+        const getData = async () => {
+            // destructuring data from response
+            const { data } = await axios.get(url);
+            console.log(data);
+            setloading(false);
+            setcoins(data);
+        }
+        getData();
+    }, [currency])
+
+
+
+
     return (
-        <div>
-            coins
-        </div>
+        <>
+            {
+                loading ? <Loading /> :
+                    <>
+                        <Header />
+                        <div className="btns">
+                            <button onClick={()=>{setcurrency('inr')}}>INR</button>
+                            <button onClick={() => {setcurrency('usd')}}>USD</button>
+                        </div>
+                        {
+
+                            coins.map((coindata, i) => {
+                                return (
+                                    <div className="coin-card" key={i}>
+                                        <div className="coin-img"><img src={coindata.image} height={60}/></div>
+                                        <div className="coin-name">{coindata.name}</div>
+                                        <div className="coin-price">{currencySymbol} {coindata.current_price.toFixed(0)}</div>
+                                        <div className="coin-profitloss" style={coindata.price_change_percentage_24h > 0 ? { color: "green"} : {color:"red"}}>{coindata.price_change_percentage_24h}</div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </>
+            }
+        </>
     )
 }
 
